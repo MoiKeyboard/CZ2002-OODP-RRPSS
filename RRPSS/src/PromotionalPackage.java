e3c43890a64985d4e587e5f6b73102cba654f8f1
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2019-03-21 14:37:03 +0800

    Updated promoPackage

--- RRPSS/src/PromotionalPackage.java
+++ RRPSS/src/PromotionalPackage.java
@@ -1,4 +1,18 @@
 
 public class PromotionalPackage {
+	String promoName;
+	String description;
+	MenuItem[] promoPackageArr;
+
+	public PromotionalPackage(String promoName, String description, int promoPackageSize) {
+		super();
+		this.promoName = promoName;
+		this.description = description;
+		promoPackageArr = new MenuItem[promoPackageSize];
+	}
+
+	void addMenuItem(String foodName) {
+
+	}
 
 }
