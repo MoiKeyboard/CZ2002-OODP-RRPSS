1e60c05c280dbcb69fe0cba87fe3aaf6868b1250
MoiKeyboard <43774527+MoiKeyboard@users.noreply.github.com>
2019-04-17 15:32:14 +0800

    Updated

--- RRPSS/src/Entity/Alacarte.java
+++ RRPSS/src/Entity/Alacarte.java
@@ -69,14 +69,11 @@ public class Alacarte extends Menu implements Serializable {
 	public String toString() {
 		String menuItemDetails = "";
 		String display = "";
-		
+
 		display += String.format("%-10s : %-22s %5.2f\n", getCategory(), getName(), getPrice());
 		menuItemDetails += display;
-		menuItemDetails += "\n"+getDescription();
+		menuItemDetails += "\n" + getDescription();
 		menuItemDetails += "\n------------------------------------------";
-		
-		
-		//"PROMO: %-22s %5.2f\n", getName(), getPrice()
 		return menuItemDetails;
 	}
 
